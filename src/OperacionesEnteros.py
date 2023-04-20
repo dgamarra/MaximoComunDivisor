cf8ef05fa0c280253a98328e0ff48cac35c68592
dgamarra <dgamarra@continental.edu.pe>
2023-04-19 20:50:58 -0500

    MCD de dos números (verde)

--- src/OperacionesEnteros.py
+++ src/OperacionesEnteros.py
@@ -1,3 +1,8 @@
 class OperacionesEnteros:
     def MCD(self, numero1, numero2):
-        return None
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
