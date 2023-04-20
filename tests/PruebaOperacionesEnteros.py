fe4fd0e00457edbaa75e751442d22a5b40aee7d5
dgamarra <dgamarra@continental.edu.pe>
2023-04-19 20:15:23 -0500

    Configurando la clase de prueba PruebaOperacionesEnteros

--- tests/PruebaOperacionesEnteros.py
+++ tests/PruebaOperacionesEnteros.py
@@ -1,5 +1,5 @@
 import unittest
 
 class PruebaOperacionesEnteros(unittest.TestCase):
-    def test_something(self):
+    def test_mcd(self):
         self.assertEqual(True, False)  # add assertion here
